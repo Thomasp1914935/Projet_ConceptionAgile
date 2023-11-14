ffa7e1335590a927fe7a9164e43ba9c1135e4c86
ThomasBelin <thobelin@gmail.com>
2023-11-14 15:40:40 +0100

    changement main et ajout fichier json
    ajout fonctionnalité rentrer nb joueur avec vérification du nombre saisi. J'ai pas reussi à faire saisir le nom avec l'ajout de celui ci dans le data frame

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import tkinter as tk
 
 # Créer la fenêtre principale
 fenetre_accueil = tk.Tk()
+fenetre_accueil.geometry ( '600x200' )
 
 # Créer un bouton
 bouton_lancer_partie = tk.Button(fenetre_accueil, text="Lancer la partie")
@@ -13,6 +14,7 @@ bouton_lancer_partie.pack()
 def lancer_partie():
     fenetre_accueil.withdraw()        # Fermer la fenêtre principale
     fenetre_config_partie.deiconify() # Ouvrir la fenêtre secondaire
+    fenetre_config_partie.geometry ( '600x200' )
 
 # Attacher la fonction au bouton
 bouton_lancer_partie.config(command=lancer_partie)
@@ -24,13 +26,11 @@ fenetre_config_partie = tk.Toplevel(fenetre_accueil)
 champ_texte = tk.Entry(fenetre_config_partie)
 champ_texte.pack()
 
-# Définir la fonction de validation
-def valider(entree):
-    try:
-        int(entree.get())
-        return True
-    except ValueError:
-        return False
+# Créer un widget Label
+label_message = tk.Label(fenetre_config_partie, text="")
+label_message.pack()
+
+
 
 # Attacher la fonction de validation au champ de texte
 champ_texte.config(validate="key", validatecommand=(champ_texte, "validate"))
@@ -39,11 +39,26 @@ champ_texte.config(validate="key", validatecommand=(champ_texte, "validate"))
 bouton_valider = tk.Button(fenetre_config_partie, text="Valider")
 bouton_valider.pack()
 
+# Définir la fonction de validation
+def valider(entree):
+    try:
+        nb_joueurs = int(entree.get())
+        return nb_joueurs >= 2 and nb_joueurs <= 10
+    except ValueError:
+        return False
+
 # Définir la fonction appelée lorsque le bouton est cliqué
 def valider_joueurs():
     # Récupérer le nombre de joueurs
     global nb_joueurs
     nb_joueurs = int(champ_texte.get())
+
+    # Vérifier que le nombre de joueurs est valide
+    if not valider(champ_texte):
+        message_erreur = "Le nombre de joueurs doit être compris entre 2 et 10."
+        label_message.config(text=message_erreur)
+        champ_texte.config(bg="red")
+        return
 
     # Créer un DataFrame de taille `nb_joueurs`
     df = pd.DataFrame(columns=['carte'], index=[f'Joueur {i}' for i in range(nb_joueurs)])
@@ -53,11 +68,17 @@ def valider_joueurs():
 
     print(df)
 
+
     # Fermer la fenêtre secondaire
     fenetre_config_partie.withdraw()
+
+    # Fermer la fenêtre principale
+    fenetre_accueil.destroy()
+
 
 # Attacher la fonction au bouton
 bouton_valider.config(command=valider_joueurs)
 
 # Afficher la fenêtre principale
 fenetre_accueil.mainloop()
+
