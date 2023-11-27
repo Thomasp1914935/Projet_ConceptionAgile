fb40d5e87c657b1dc5e309406ecf5581bffc838a
hugocollin <hugo.collin3@gmail.com>
2023-11-27 14:23:26 +0100

    Création de joueur.py
    Création de la classe Joueur

---
+++ joueur.py
@@ -0,0 +1,8 @@
+class Joueur:
+
+    def __init__(self, numero, nom):
+        self.numero = numero
+        self.nom = nom
+
+    def __repr__(self):
+        return f"Joueur {self.numero} : {self.nom}"
