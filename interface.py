6ee023eb66b75f37263232797a94eb753d8b2f25
hugocollin <hugo.collin3@gmail.com>
2023-12-08 17:53:22 +0100

    Traitement sur l'ajout de joueurs

--- interface.py
+++ interface.py
@@ -3,11 +3,18 @@ from jeu import Joueur
 import sys    # Importation du module sys pour accéder aux fonctionnalités système de Python
 import pygame # Importation de la bibliothèque Pygame pour créer des jeux et des applications multimédias
 
+# Initialisation de toutes les bibliothèques Pygame et leurs modules associés
+pygame.init()
+pygame.font.init()
+
+# Définition des variables globales
+COLOR_INACTIVE = pygame.Color('grey88')
+COLOR_ACTIVE = pygame.Color('black')
+FONT = pygame.font.Font(None, 30)
+
 class FenetreAccueil:
     # Méthode pour initialiser la fenêtre d'accueil
     def __init__(self):
-        # Initialisation de toutes les bibliothèques Pygame et leurs modules associés
-        pygame.init()
 
         # Configuration de la fenêtre accueil
         self.screen_width = 400                                                        # Largeur de la fenêtre
@@ -62,7 +69,7 @@ class FenetreAccueil:
             self.screen.blit(text, text_rect)                                                               # Affichage du texte sur le bouton
 
             pygame.display.flip() # Mise à jour de l'affichage sur l'écran
-            self.clock.tick(120)   # Contrôle de la vitesse de rafraîchissement de l'écran
+            self.clock.tick(120)  # Contrôle de la vitesse de rafraîchissement de l'écran
 
         pygame.quit() # Quitte Pygame
         sys.exit()    # Quitte le programme
@@ -86,8 +93,8 @@ class FenetreJoueurs(FenetreAccueil):
 
         # Initialisation des champs de saisie des noms des joueurs
         self.input_boxes = [
-            pygame.Rect(50, 100, 200, 30), # Champs de saisie pour le joueur 1
-            pygame.Rect(50, 150, 200, 30)  # Champs de saisie pour le joueur 2
+            InputBox(50, 100, 300, 30), # Champs de saisie pour le joueur 1
+            InputBox(50, 140, 300, 30)  # Champs de saisie pour le joueur 2
         ]
         self.joueurs = []  # Liste pour stocker les instances des joueurs
 
@@ -100,7 +107,7 @@ class FenetreJoueurs(FenetreAccueil):
         button_width = 100                                                                  # Largeur du bouton
         button_height = 40                                                                  # Hauteur du bouton
         button_x = (self.screen_width - button_width) // 2                                  # Position x du bouton
-        button_y = self.screen_height - 60                                                  # Position y du bouton
+        button_y = self.screen_height - 50                                                  # Position y du bouton
         self.validate_button = pygame.Rect(button_x, button_y, button_width, button_height) # Création du bouton valider
         border_radius = 10                                                                  # Rayon de l'arrondi des coins
         self.validate_button_radius = border_radius                                         # Ajout d'un attribut pour le rayon d'arrondi du bouton
@@ -122,15 +129,30 @@ class FenetreJoueurs(FenetreAccueil):
                     elif self.validate_button.collidepoint(event.pos):   # Vérification du clic sur le bouton valider
                         self.go_to_config_taches()                       # Appel de la méthode pour aller à la fenêtre de configuration des tâches
 
-            # Changement de curseur en fonction de l'état de survol de chaque bouton
-            if self.return_button.collidepoint(pygame.mouse.get_pos()):       # Si survol du bouton retour
-                pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
-            elif self.add_player_button.collidepoint(pygame.mouse.get_pos()): # Si survol du bouton "Ajouter un joueur"
-                pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
-            elif self.validate_button.collidepoint(pygame.mouse.get_pos()):   # Si survol du bouton valider
-                pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
-            else:                                                             # Si aucun bouton n'est survolé
-                pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+                for box in self.input_boxes:                             # Boucle pour gérer les événements sur les champs de saisie
+                    box.handle_event(event)                              # Appel de la méthode pour gérer les événements sur les champs de saisie
+
+            for box in self.input_boxes:
+                box.update()
+                box.draw(self.screen)
+
+            # Définir le curseur par défaut
+            pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+
+            # Liste de tous les boutons
+            buttons = [self.return_button, self.add_player_button, self.validate_button]
+
+            # Vérifier si la souris survole un bouton
+            for button in buttons:
+                if button.collidepoint(pygame.mouse.get_pos()):
+                    pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
+                    break
+
+            # Vérifier si la souris survole une boîte de saisie
+            for box in self.input_boxes:
+                if box.collidepoint(pygame.mouse.get_pos()):
+                    pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_IBEAM)
+                    break
                 
             # Paramètres d'affichage du bouton retour
             pygame.draw.rect(self.screen, self.button_color, self.return_button, border_radius=self.return_button_radius) # Dessin du bouton avec la couleur spécifiée
@@ -166,23 +188,28 @@ class FenetreJoueurs(FenetreAccueil):
         fenetre_accueil.run()              # Exécution de la méthode run de FenetreAccueil pour revenir à l'accueil
 
     def add_player(self):
-        # Ajout d'un joueur à la liste des joueurs avec les noms saisis dans les champs de texte
-        new_player_names = []
-        for index, box in enumerate(self.input_boxes):
-            # Si le champ de saisie est vide, ajoutez une chaîne vide pour ce joueur
-            if not box.collidepoint(pygame.mouse.get_pos()):  
-                new_player_names.append("")
-            else:
-                new_player_names.append("")  # Vous pouvez utiliser ici votre propre logique pour obtenir le texte saisi
+        # Vérifiez si le nombre de boîtes de saisie est inférieur à 10
+        if len(self.input_boxes) < 10:
+            # Calculez la position y de la nouvelle boîte de saisie
+            y = 100 + len(self.input_boxes) * 40  # 100 est la position y de départ, 40 est l'espace entre les boîtes de saisie
 
-        for name in new_player_names:
-            if name:
-                nouveau_joueur = Joueur(len(self.joueurs) + 1, name)
-                self.joueurs.append(nouveau_joueur)
+            # Créez une nouvelle boîte de saisie
+            new_box = InputBox(50, y, 300, 30)
+
+            # Ajoutez la nouvelle boîte de saisie à la liste
+            self.input_boxes.append(new_box)
+
+            # Déplacez le bouton "Ajouter un joueur" vers le bas
+            self.add_player_button.y = y + 50  # 50 est l'espace entre la dernière boîte de saisie et le bouton
      
-    
     # Méthode pour aller à la fenêtre de configuration des tâches
     def go_to_config_taches(self):
+
+        # Parcourez la liste des boîtes de saisie et créez une nouvelle instance de Joueur pour chaque boîte de saisie
+        for i, box in enumerate(self.input_boxes):
+            self.joueurs.append(Joueur(i+1, box.text))  # Créez une nouvelle instance de Joueur avec le numéro du joueur et le texte de la boîte de saisie
+            print(Joueur(i+1, box.text))                # [DEBUG]
+
         self.is_running = False          # Met fin à la boucle de la fenêtre de configuration des joueurs
         fenetre_taches = FenetreTaches() # Création d'une nouvelle instance de FenetreTaches
         fenetre_taches.run()             # Exécution de la méthode run de FenetreTaches pour aller à la configuration des tâches
@@ -232,3 +259,44 @@ class FenetreTaches(FenetreAccueil):
         self.is_running = False            # Met fin à la boucle de la fenêtre de configuration des tâches
         fenetre_joueurs = FenetreJoueurs() # Création d'une nouvelle instance de FenetreJoueurs
         fenetre_joueurs.run()              # Exécution de la méthode run de FenetreJoueurs pour revenir à la configuration des joueurs
+
+class InputBox:
+
+    def __init__(self, x, y, w, h, text=''):
+        self.rect = pygame.Rect(x, y, w, h)
+        self.color = COLOR_INACTIVE
+        self.text = text
+        self.txt_surface = FONT.render(text, True, self.color)
+        self.active = False
+        self.max_length = 15  # Ajoutez cette ligne pour définir la longueur maximale du texte
+    
+    def collidepoint(self, pos):
+        return self.rect.collidepoint(pos)
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.rect.collidepoint(event.pos):
+                self.active = not self.active
+            else:
+                self.active = False
+            self.color = COLOR_ACTIVE if self.active else COLOR_INACTIVE
+        if event.type == pygame.KEYDOWN:
+            if self.active:
+                if event.key == pygame.K_RETURN:
+                    print(self.text)
+                    self.text = ''
+                elif event.key == pygame.K_BACKSPACE:
+                    self.text = self.text[:-1]
+                else:
+                    # Ajoutez cette condition pour vérifier la longueur du texte avant d'ajouter un nouveau caractère
+                    if len(self.text) < self.max_length:
+                        self.text += event.unicode
+                self.txt_surface = FONT.render(self.text, True, self.color)
+
+    def update(self):
+        width = max(250, self.txt_surface.get_width()+10)
+        self.rect.w = width
+
+    def draw(self, screen):
+        screen.blit(self.txt_surface, (self.rect.x+5, self.rect.y+5))
+        pygame.draw.rect(screen, self.color, self.rect, 2)    
--- main.py
+++ main.py
@@ -4,4 +4,3 @@ if __name__ == "__main__":
     # Lancement de la fenêtre Accueil
     fenetre_accueil = FenetreAccueil()
     fenetre_accueil.run()
-    
